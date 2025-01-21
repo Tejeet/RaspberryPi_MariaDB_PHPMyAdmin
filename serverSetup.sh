4dc2a88ae980d7da55b30b2a59b1f152215672dd
Tejeet Magar <Tejeetm@gmail.com>
2025-01-21 11:57:06 +0530

    Update serverSetup.sh
    Added Supervisord install command at 2nd Setp

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "\nInstalling MYSQL Apache and PHPmyAdmin Server"
+echo "\nInstalling MYSQL, Apache, and phpMyAdmin Server"
 
 # Define the blue color
 BLUE='\033[34m'
@@ -11,15 +11,21 @@ echo -e "${BLUE}Updating and upgrading system packages...${NC}"
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# Step 1: Instal Github
-echo -e "${BLUE}Installing github...${NC}"
+# Step 1: Install GitHub
+echo -e "${BLUE}Installing GitHub...${NC}"
 sudo apt-get install git -y
 
-# Step 2: Install MariaDB (MySQL Alternative)
+# Step 2: Install Supervisor
+echo -e "${BLUE}Installing Supervisor...${NC}"
+sudo apt install supervisor -y
+echo -e "${BLUE}Checking Supervisor version...${NC}"
+sudo supervisorctl version
+
+# Step 3: Install MariaDB (MySQL Alternative)
 echo -e "${BLUE}Installing MariaDB server and client...${NC}"
 sudo apt-get install mariadb-server mariadb-client -y
 
-# Step 3: Install Apache Web Server
+# Step 4: Install Apache Web Server
 echo -e "${BLUE}Installing Apache web server...${NC}"
 sudo apt-get install apache2 -y
 
@@ -27,7 +33,7 @@ sudo apt-get install apache2 -y
 echo -e "${BLUE}Enabling Apache rewrite module...${NC}"
 sudo a2enmod rewrite
 
-# Step 4: Install PHP and Required Modules
+# Step 5: Install PHP and Required Modules
 echo -e "${BLUE}Installing PHP and necessary modules...${NC}"
 sudo apt-get install php libapache2-mod-php php-mysql -y
 
@@ -35,22 +41,22 @@ sudo apt-get install php libapache2-mod-php php-mysql -y
 echo -e "${BLUE}Restarting Apache...${NC}"
 sudo systemctl restart apache2
 
-# Step 5: Install phpMyAdmin
+# Step 6: Install phpMyAdmin
 echo -e "${BLUE}Installing phpMyAdmin...${NC}"
 sudo apt-get install phpmyadmin -y
 
-# Step 6: Configure Apache to Include phpMyAdmin
+# Step 7: Configure Apache to Include phpMyAdmin
 echo -e "${BLUE}Configuring Apache to include phpMyAdmin...${NC}"
 sudo sh -c "echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf"
 
-# Step 7: Restart Apache to Apply Changes
+# Step 8: Restart Apache to Apply Changes
 echo -e "${BLUE}Restarting Apache to apply configuration changes...${NC}"
 sudo systemctl restart apache2
 
-# Step 8: Add MySQL user in DB
+# Step 9: Add MySQL user in DB
 echo -e "${BLUE}Configuring MySQL root user...${NC}"
 
-# Step 9: Modify User Permissions
+# Step 10: Modify User Permissions
 echo -e "${BLUE}Modify MYSQL user Permissions...${NC}"
 sudo mysql -u root <<EOF
 DROP USER IF EXISTS 'root'@'localhost';
